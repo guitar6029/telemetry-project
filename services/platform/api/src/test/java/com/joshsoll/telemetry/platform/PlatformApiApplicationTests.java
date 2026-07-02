74409a1ad3dbde9c001b4e4439383cdf05c1f6c4
Josh <jsdev305@gmail.com>
2026-07-02 19:54:45 -0400

    init maven

---
+++ services/platform/api/src/test/java/com/joshsoll/telemetry/platform/PlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshsoll.telemetry.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
