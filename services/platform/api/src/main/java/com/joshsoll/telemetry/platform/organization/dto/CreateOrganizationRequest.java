79a18171a98d52e1be7d0065a327ace4998ed964
Josh <jsdev305@gmail.com>
2026-07-03 18:11:49 -0400

    add Org Controller , add Post request for create organization

---
+++ services/platform/api/src/main/java/com/joshsoll/telemetry/platform/organization/dto/CreateOrganizationRequest.java
@@ -0,0 +1,34 @@
+package com.joshsoll.telemetry.platform.organization.dto;
+
+import com.joshsoll.telemetry.platform.organization.constants.OrganizationConstants;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class CreateOrganizationRequest {
+
+    @NotBlank
+    // fix the magic numbers
+    @Size(min = OrganizationConstants.NAME_MIN_LENGTH, max = OrganizationConstants.NAME_MAX_LENGTH)
+    private String name;
+
+    @NotBlank
+    @Size(min = OrganizationConstants.SLUG_MIN_LENGTH, max = OrganizationConstants.SLUG_MAX_LENGTH)
+    private String slug;
+
+    public CreateOrganizationRequest() {
+    }
+
+    public CreateOrganizationRequest(String name, String slug) {
+        this.name = name;
+        this.slug = slug;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+}
---
+++ services/platform/api/src/main/java/com/joshsoll/telemetry/platform/organization/repository/OrganizationRepository.java
@@ -0,0 +1,11 @@
+package com.joshsoll.telemetry.platform.organization.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.joshsoll.telemetry.platform.organization.entity.Organization;
+
+public interface OrganizationRepository extends JpaRepository<Organization, UUID> {
+
+}
