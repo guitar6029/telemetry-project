bfc916bce08c88a4f77f85c8796830d6debccc89
Josh <jsdev305@gmail.com>
2026-07-07 15:36:05 -0400

    test: add OrganizationService unit tests

--- services/platform/api/src/test/java/com/joshsoll/telemetry/platform/organization/service/OrganizationServiceTest.java
+++ services/platform/api/src/test/java/com/joshsoll/telemetry/platform/organization/service/OrganizationServiceTest.java
@@ -6,15 +6,19 @@ import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.joshsoll.telemetry.platform.organization.dto.CreateOrganizationRequest;
 import com.joshsoll.telemetry.platform.organization.dto.OrganizationResponse;
 import com.joshsoll.telemetry.platform.organization.entity.Organization;
 import com.joshsoll.telemetry.platform.organization.repository.OrganizationRepository;
@@ -62,5 +66,31 @@ public class OrganizationServiceTest {
         assertThrows(
                 NoSuchElementException.class,
                 () -> organizationService.getOrganization(id));
+    }
+
+    @Test
+    void shouldCreateANewOrganization() {
+
+        // create dummy organization
+        Organization organization = new Organization("MyOrganization", "my-organization", Instant.now(), Instant.now());
+
+        // call org repo to save this organization
+        when(organizationRepository.save(any(Organization.class)))
+                .thenReturn(organization);
+
+        CreateOrganizationRequest request = new CreateOrganizationRequest(
+                "MyOrganization",
+                "my-organization");
+
+        OrganizationResponse response = organizationService.createOrganization(request);
+
+        assertEquals(request.getName(), response.name());
+        assertEquals(request.getSlug(), response.slug());
+        assertNotNull(response.createdAt());
+        assertNotNull(response.updatedAt());
+
+        verify(organizationRepository)
+                .save(any(Organization.class));
+
     }
 }
