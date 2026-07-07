237efba18291199ee5873356f7990557de5bb57a
Josh <jsdev305@gmail.com>
2026-07-07 10:24:57 -0400

    test: add unit tests for OrganizationService

---
+++ services/platform/api/src/test/java/com/joshsoll/telemetry/platform/organization/service/OrganizationServiceTest.java
@@ -0,0 +1,59 @@
+package com.joshsoll.telemetry.platform.organization.service;
+
+import java.time.Instant;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.when;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.joshsoll.telemetry.platform.organization.dto.OrganizationResponse;
+import com.joshsoll.telemetry.platform.organization.entity.Organization;
+import com.joshsoll.telemetry.platform.organization.repository.OrganizationRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class OrganizationServiceTest {
+    @Mock
+    private OrganizationRepository organizationRepository;
+
+    @InjectMocks
+    private OrganizationService organizationService;
+
+    @Test
+    void shouldReturnOrganizationWhenIdExists() {
+
+        Organization organization = new Organization("OpenAi", "openai", Instant.now(), Instant.now());
+
+        when(organizationRepository.findById(organization.getId()))
+                .thenReturn(Optional.of(organization));
+
+        OrganizationResponse response = organizationService.getOrganization(organization.getId());
+
+        assertEquals(organization.getId(), response.id());
+        assertEquals(organization.getName(), response.name());
+        assertEquals(organization.getSlug(), response.slug());
+
+    }
+
+    @Test
+    void shouldThrowWhenOrganizationDoesNotExist() {
+
+        // Arrange
+        UUID id = UUID.randomUUID();
+
+        when(organizationRepository.findById(id))
+                .thenReturn(Optional.empty());
+
+        // Act + Assert
+        assertThrows(
+                NoSuchElementException.class,
+                () -> organizationService.getOrganization(id));
+    }
+}
