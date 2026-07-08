ba0be0f5e349a5e3664e75429b40c938fb058965
Josh <jsdev305@gmail.com>
2026-07-08 16:59:34 -0400

    used Page helpers to get total pages and elements

--- services/platform/api/src/main/java/com/joshsoll/telemetry/platform/organization/service/OrganizationService.java
+++ services/platform/api/src/main/java/com/joshsoll/telemetry/platform/organization/service/OrganizationService.java
@@ -79,7 +79,7 @@ public class OrganizationService {
                 "",
                 page,
                 size,
-                responses.size(),
-                1);
+                organizations.getTotalElements(),
+                organizations.getTotalPages());
     }
 }
