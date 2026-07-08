54fc9d96479148eb384be8551efd31bf94cb7803
Josh <jsdev305@gmail.com>
2026-07-08 00:02:01 -0400

    add mock test for shouldCreateOrganization

--- services/platform/api/src/test/java/com/joshsoll/telemetry/platform/organization/controller/OrganizationControllerTest.java
+++ services/platform/api/src/test/java/com/joshsoll/telemetry/platform/organization/controller/OrganizationControllerTest.java
@@ -3,16 +3,24 @@ package com.joshsoll.telemetry.platform.organization.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.joshsoll.telemetry.platform.organization.dto.CreateOrganizationRequest;
 import com.joshsoll.telemetry.platform.organization.dto.OrganizationResponse;
 import com.joshsoll.telemetry.platform.organization.service.OrganizationService;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import tools.jackson.databind.ObjectMapper;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 import java.time.Instant;
 import java.util.UUID;
@@ -28,13 +36,44 @@ class OrganizationControllerTest {
     @Test
     void shouldReturnOrganizationById() throws Exception {
         UUID id = UUID.randomUUID();
-        OrganizationResponse response = new OrganizationResponse(id, "TestDummyResponse", "test-dummy-response",
-                Instant.now(), Instant.now());
 
-        when(organizationService.getOrganization(id)).thenReturn(response);
+        OrganizationResponse response = new OrganizationResponse(
+                id,
+                "TestDummyResponse",
+                "test-dummy-response",
+                Instant.now(),
+                Instant.now());
 
-        mockMvc.perform(get("/api/v1/organizations/{id}", id)).andExpect(status().isOk())
-                // .andDo(print())
+        when(organizationService.getOrganization(id))
+                .thenReturn(response);
+
+        mockMvc.perform(get("/api/v1/organizations/{id}", id))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.name").value(response.name()))
+                .andExpect(jsonPath("$.data.slug").value(response.slug()));
+    }
+
+    @Test
+    void shouldCreateOrganization() throws Exception {
+
+        UUID id = UUID.randomUUID();
+
+        CreateOrganizationRequest request = new CreateOrganizationRequest("TestDummyResponse", "test-dummy-response");
+
+        OrganizationResponse response = new OrganizationResponse(
+                id,
+                request.getName(),
+                request.getSlug(),
+                Instant.now(),
+                Instant.now());
+
+        when(organizationService.createOrganization(any(CreateOrganizationRequest.class))).thenReturn(response);
+
+        final ObjectMapper mapper = new ObjectMapper();
+
+        mockMvc.perform(post("/api/v1/organizations").contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(request))).andExpect(status().isCreated())
+                .andDo(print())
                 .andExpect(jsonPath("$.data.name").value(response.name()))
                 .andExpect(jsonPath("$.data.slug").value(response.slug()));
     }
