8fede202af4ed9a8b0b602ff74d24e38c886ef31
Josh <jsdev305@gmail.com>
2026-07-08 14:53:57 -0400

    add Device Constants

---
+++ services/platform/api/src/main/java/com/joshsoll/telemetry/platform/device/constants/DeviceConstants.java
@@ -0,0 +1,12 @@
+package com.joshsoll.telemetry.platform.device.constants;
+
+public class DeviceConstants {
+    public static final int NAME_MIN_LENGTH = 2;
+    public static final int NAME_MAX_LENGTH = 50;
+
+    public static final int MODEL_MIN_LENGTH = 1;
+    public static final int MODEL_MAX_LENGTH = 50;
+
+    private DeviceConstants() {
+    }
+}
