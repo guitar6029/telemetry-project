29c6aa5ea39a677016d5bc2f39c78c40ccd37904
Josh <jsdev305@gmail.com>
2026-07-09 18:52:29 -0400

    added a helper method to HierarchyRepo to check if node exists by org, parent node and node name

--- services/platform/api/src/main/java/com/joshsoll/telemetry/platform/hierarchy/repository/HierarchyNodeRepository.java
+++ services/platform/api/src/main/java/com/joshsoll/telemetry/platform/hierarchy/repository/HierarchyNodeRepository.java
@@ -5,7 +5,10 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.joshsoll.telemetry.platform.hierarchy.entity.HierarchyNode;
+import com.joshsoll.telemetry.platform.organization.entity.Organization;
 
 public interface HierarchyNodeRepository extends JpaRepository<HierarchyNode, UUID> {
 
+    boolean existsByOrganizationAndParentNodeAndName(Organization organization, HierarchyNode parentNode,
+            String nodeName);
 }
