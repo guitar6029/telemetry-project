8a0370c6a4a03433d9e67e9dda24196b3d8d2229
Josh <jsdev305@gmail.com>
2026-07-09 22:04:40 -0400

    add getHierarchyNodeById

--- services/platform/api/src/main/java/com/joshsoll/telemetry/platform/hierarchy/controller/HierarchyNodeController.java
+++ services/platform/api/src/main/java/com/joshsoll/telemetry/platform/hierarchy/controller/HierarchyNodeController.java
@@ -1,7 +1,11 @@
 package com.joshsoll.telemetry.platform.hierarchy.controller;
 
+import java.util.UUID;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,5 +35,12 @@ public class HierarchyNodeController {
         ApiResponse<HierarchyNodeResponse> response = new ApiResponse<>(node, "Hierarchy node added successfully");
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<HierarchyNodeResponse>> getHierarchyNodeById(@PathVariable UUID id) {
+        HierarchyNodeResponse node = hierarchyNodeService.getHierarchyNodeById(id);
+        ApiResponse<HierarchyNodeResponse> response = new ApiResponse<>(node, "");
+        return ResponseEntity.ok(response);
     }
 }
