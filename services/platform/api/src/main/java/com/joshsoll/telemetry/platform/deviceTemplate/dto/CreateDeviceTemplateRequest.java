6de26fcd5e988b45df990ace2d4e2c49e5eb6a1f
Josh <jsdev305@gmail.com>
2026-07-12 20:53:57 -0400

    add template request

---
+++ services/platform/api/src/main/java/com/joshsoll/telemetry/platform/deviceTemplate/dto/CreateDeviceTemplateRequest.java
@@ -0,0 +1,43 @@
+package com.joshsoll.telemetry.platform.deviceTemplate.dto;
+
+import java.util.UUID;
+
+import com.joshsoll.telemetry.platform.deviceTemplate.constants.DeviceTemplateConstants;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class CreateDeviceTemplateRequest {
+
+    @NotBlank
+    @Size(min = DeviceTemplateConstants.NAME_MIN_LENGTH, max = DeviceTemplateConstants.NAME_MAX_LENGTH)
+    private String name;
+
+    private String description;
+
+    @NotNull
+    private UUID organizationId;
+
+    public CreateDeviceTemplateRequest() {
+    }
+
+    public CreateDeviceTemplateRequest(String name, String description, UUID organizationId) {
+        this.name = name;
+        this.description = description;
+        this.organizationId = organizationId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public UUID getOrganizationId() {
+        return organizationId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+}
