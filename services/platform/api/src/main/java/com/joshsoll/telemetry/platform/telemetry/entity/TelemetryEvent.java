90844a69d608df43e58210f4bcb737f858f4cf8f
Josh <jsdev305@gmail.com>
2026-07-14 11:07:12 -0400

    feat: add telemetry event entity

---
+++ services/platform/api/src/main/java/com/joshsoll/telemetry/platform/telemetry/entity/TelemetryEvent.java
@@ -0,0 +1,71 @@
+package com.joshsoll.telemetry.platform.telemetry.entity;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import com.joshsoll.telemetry.platform.device.entity.Device;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "telemetry_events")
+public class TelemetryEvent {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @ManyToOne
+    private Device device;
+
+    private String rawPayload;
+
+    private Instant createdAt;
+
+    private Instant receivedAt;
+
+    protected TelemetryEvent() {
+    }
+
+    public TelemetryEvent(
+
+            Device device,
+
+            String rawPayload,
+
+            Instant createdAt,
+
+            Instant receivedAt
+
+    ) {
+
+        this.device = device;
+        this.rawPayload = rawPayload;
+        this.createdAt = createdAt;
+        this.receivedAt = receivedAt;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public UUID getDeviceId() {
+        return device.getId();
+    }
+
+    public String getRawPayload() {
+        return rawPayload;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getReceivedAt() {
+        return receivedAt;
+    }
+}
