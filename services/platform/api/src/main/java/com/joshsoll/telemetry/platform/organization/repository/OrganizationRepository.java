f3bdd899c94f6aa043583bccc0686668f1cac0c2
Josh <jsdev305@gmail.com>
2026-07-17 12:41:01 -0400

    added query helper to check if slug already exists

--- services/platform/api/src/main/java/com/joshsoll/telemetry/platform/organization/repository/OrganizationRepository.java
+++ services/platform/api/src/main/java/com/joshsoll/telemetry/platform/organization/repository/OrganizationRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.joshsoll.telemetry.platform.organization.entity.Organization;
 
 public interface OrganizationRepository extends JpaRepository<Organization, UUID> {
-
+    boolean existsBySlug(String slug);
 }
