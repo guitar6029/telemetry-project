a824ff5884ae04d8bad4ec1a0ac4b2b78190cc8a
Josh <jsdev305@gmail.com>
2026-07-18 15:43:43 -0400

    add constant min and max length for some of the device's properties

--- services/platform/api/src/main/java/com/joshsoll/telemetry/platform/device/constants/DeviceConstants.java
+++ services/platform/api/src/main/java/com/joshsoll/telemetry/platform/device/constants/DeviceConstants.java
@@ -7,6 +7,17 @@ public class DeviceConstants {
     public static final int MODEL_MIN_LENGTH = 1;
     public static final int MODEL_MAX_LENGTH = 50;
 
+    public static final int MANUFACTURER_MIN_LENGTH = 2;
+    public static final int MANUFACTURER_MAX_LENGTH = 50;
+
+    public static final int SERIAL_MIN_LENGTH = 2;
+    public static final int SERIAL_MAX_LENGTH = 50;
+
+    public static final int FIRMWARE_VERSION_MIN_LENGTH = 2;
+    public static final int FIRMWARE_VERSION_MAX_LENGTH = 50;
+
+    public static final int DEVICE_STATUS_MAX_LENGTH = 20;
+
     private DeviceConstants() {
     }
 }
