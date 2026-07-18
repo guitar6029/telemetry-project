7258938c5fb67f1e573e8573da06e55c6e26750e
Josh <jsdev305@gmail.com>
2026-07-18 13:01:06 -0400

    updated Device Template entity decorators

--- services/platform/api/src/main/java/com/joshsoll/telemetry/platform/deviceTemplate/entity/DeviceTemplate.java
+++ services/platform/api/src/main/java/com/joshsoll/telemetry/platform/deviceTemplate/entity/DeviceTemplate.java
@@ -3,11 +3,17 @@ package com.joshsoll.telemetry.platform.deviceTemplate.entity;
 import java.time.Instant;
 import java.util.UUID;
 
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Generated;
+
+import com.joshsoll.telemetry.platform.deviceTemplate.constants.DeviceTemplateConstants;
 import com.joshsoll.telemetry.platform.organization.entity.Organization;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
@@ -16,18 +22,24 @@ import jakarta.persistence.Table;
 public class DeviceTemplate {
 
     @Id
-    @GeneratedValue
+    @Generated
+    @ColumnDefault("gen_random_uuid()")
     private UUID id;
 
+    @Column(nullable = false, length = DeviceTemplateConstants.NAME_MAX_LENGTH)
     private String name;
+
+    @Column(length = DeviceTemplateConstants.DESCRIPTION_MAX_LENGTH)
     private String description;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "organization_id", nullable = false)
     private Organization organization;
 
     private boolean archived;
 
     private Instant createdAt;
+
     private Instant updatedAt;
 
     protected DeviceTemplate() {
