a433dfd56fdbf7a7887aa8ddb11ff59dd59c36ae
Josh <jsdev305@gmail.com>
2026-07-20 15:49:52 -0400

    add ResponseFactory

--- services/platform/api/src/main/java/com/joshsoll/telemetry/platform/hierarchy/controller/HierarchyNodeController.java
+++ services/platform/api/src/main/java/com/joshsoll/telemetry/platform/hierarchy/controller/HierarchyNodeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.joshsoll.telemetry.platform.common.response.ApiResponse;
+import com.joshsoll.telemetry.platform.common.response.ResponseFactory;
 import com.joshsoll.telemetry.platform.hierarchy.dto.CreateHierarchyNodeRequest;
 import com.joshsoll.telemetry.platform.hierarchy.dto.HierarchyNodeResponse;
 import com.joshsoll.telemetry.platform.hierarchy.service.HierarchyNodeService;
@@ -24,6 +25,7 @@ import jakarta.validation.Valid;
 public class HierarchyNodeController {
 
     private final HierarchyNodeService hierarchyNodeService;
+    private final String DOMAIN_NAME = "Hierarchy Node";
 
     public HierarchyNodeController(HierarchyNodeService hierarchyNodeService) {
         this.hierarchyNodeService = hierarchyNodeService;
@@ -34,16 +36,13 @@ public class HierarchyNodeController {
     public ResponseEntity<ApiResponse<HierarchyNodeResponse>> createHierarchyNode(
             @Valid @RequestBody CreateHierarchyNodeRequest request) {
         HierarchyNodeResponse node = hierarchyNodeService.createHierarchyNode(request);
-        ApiResponse<HierarchyNodeResponse> response = new ApiResponse<>(node, "Hierarchy node added successfully");
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        return ResponseFactory.created(node, DOMAIN_NAME);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ApiResponse<HierarchyNodeResponse>> getHierarchyNodeById(@PathVariable UUID id) {
         HierarchyNodeResponse node = hierarchyNodeService.getHierarchyNodeById(id);
-        ApiResponse<HierarchyNodeResponse> response = new ApiResponse<>(node, "");
-        return ResponseEntity.ok(response);
+        return ResponseFactory.ok(node, DOMAIN_NAME);
     }
 
     @GetMapping
@@ -51,10 +50,7 @@ public class HierarchyNodeController {
             @RequestParam UUID organizationId) {
 
         List<HierarchyNodeResponse> nodes = hierarchyNodeService.getHierarchyByOrganization(organizationId);
-
-        ApiResponse<List<HierarchyNodeResponse>> response = new ApiResponse<>(nodes, "");
-
-        return ResponseEntity.ok(response);
+        return ResponseFactory.ok(nodes, null);
 
     }
 
@@ -62,9 +58,6 @@ public class HierarchyNodeController {
     public ResponseEntity<ApiResponse<List<HierarchyNodeResponse>>> getHierarchyChildNodesByparent(
             @PathVariable UUID nodeId) {
         List<HierarchyNodeResponse> nodes = hierarchyNodeService.getChildNodesByParentNodeId(nodeId);
-
-        ApiResponse<List<HierarchyNodeResponse>> response = new ApiResponse<>(nodes, "");
-
-        return ResponseEntity.ok(response);
+        return ResponseFactory.ok(nodes, null);
     }
 }
