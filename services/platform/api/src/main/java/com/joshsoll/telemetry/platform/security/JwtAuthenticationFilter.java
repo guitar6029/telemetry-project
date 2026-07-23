2e74e877c5e098d3d60ed28f03595562d4404985
Josh Soll <jsdev305@gmail.com>
2026-07-23 17:06:03 -0400

    feat(security): validate bearer tokens in JWT filter

--- services/platform/api/src/main/java/com/joshsoll/telemetry/platform/security/JwtAuthenticationFilter.java
+++ services/platform/api/src/main/java/com/joshsoll/telemetry/platform/security/JwtAuthenticationFilter.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.joshsoll.telemetry.platform.auth.service.JwtService;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,12 +14,34 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final String BEARER_PREFIX = "Bearer ";
+    private final JwtService jwtService;
+
+    public JwtAuthenticationFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
     @Override
     protected void doFilterInternal(
             HttpServletRequest request,
             HttpServletResponse response,
             FilterChain filterChain) throws ServletException, IOException {
-        System.out.println("JWT Filter Executed");
+
+        String authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader == null || !authorizationHeader.startsWith(BEARER_PREFIX)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authorizationHeader.substring(BEARER_PREFIX.length());
+
+        if (!jwtService.validateToken(token)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         filterChain.doFilter(request, response);
     }
 }
