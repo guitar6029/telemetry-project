4f5cf0aba100d622617c58ec72b99d20a0c6a387
Josh Soll <jsdev305@gmail.com>
2026-07-24 16:32:43 -0400

    add auth routes to the root routes manager

--- services/platform/web/src/app/app.routes.ts
+++ services/platform/web/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AUTH_ROUTES } from './features/auth/auth.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'auth',
+        children: AUTH_ROUTES
+    }
+];
