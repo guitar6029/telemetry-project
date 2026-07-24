0bb0dc91e6362ed221ddee2fc9d89fe01604bb34
Josh Soll <jsdev305@gmail.com>
2026-07-23 23:16:25 -0400

    feat(web): implement authentication service

---
+++ services/platform/web/src/app/features/auth/services/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { LoginRequest } from "../dto/login-request";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { LoginResponse } from "../dto/login-response";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthService {
+
+    constructor(private http: HttpClient) {
+
+    }
+
+    login(request: LoginRequest): Observable<LoginResponse> {
+        return this.http.post<LoginResponse>(
+            'api/v1/auth/login',
+            request
+        )
+    }
+
+
+}
