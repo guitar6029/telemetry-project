d471fc018fbf9194b9b9d297e5c31f4cc2590747
Josh Soll <jsdev305@gmail.com>
2026-07-23 20:24:07 -0400

    feat(web): setup Angular Material and frontend structure

---
+++ services/platform/web/src/app/shared/material/material.ts
@@ -0,0 +1,5 @@
+export * from '@angular/material/button';
+export * from '@angular/material/card';
+export * from '@angular/material/input';
+export * from '@angular/material/icon';
+export * from '@angular/material/form-field';
