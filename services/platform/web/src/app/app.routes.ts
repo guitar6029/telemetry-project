229520f022a62d1f413712443548b19122c7d85c
Josh Soll <jsdev305@gmail.com>
2026-07-24 20:13:43 -0400

    add base dashboard page

--- services/platform/web/src/app/app.routes.ts
+++ services/platform/web/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { AUTH_ROUTES } from './features/auth/auth.routes';
+import { DashboardComponent } from './features/dashboard/pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
     {
         path: 'auth',
         children: AUTH_ROUTES
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent
     }
 ];
