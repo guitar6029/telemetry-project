8ff30f1c9aff3e285b1285b80a318afdd74b30f1
Josh Soll <jsdev305@gmail.com>
2026-07-25 13:45:05 -0400

    feat(web): add organization list pagination

--- services/platform/web/src/app/features/organization/pages/organization-list/organization-list.component.ts
+++ services/platform/web/src/app/features/organization/pages/organization-list/organization-list.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit, signal } from "@angular/core";
 import { OrganizationResponse } from "../../dto/organization-response";
 import { OrganizationService } from "../../services/organization.service";
 import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule, PageEvent } from "@angular/material/paginator";
 
 @Component({
     selector: 'app-organization-list',
     imports: [
-        MatTableModule
+        MatTableModule,
+        MatPaginatorModule
     ],
     templateUrl: './organization-list.component.html',
     styleUrl: './organization-list.component.scss'
@@ -14,6 +16,10 @@ import { MatTableModule } from "@angular/material/table";
 
 export class OrganizationListComponent implements OnInit {
     organizations = signal<OrganizationResponse[]>([]);
+
+    page = signal(0);
+    pageSize = signal(10);
+    total = signal(0);
 
     displayedColumns: string[] = [
         'name',
@@ -30,10 +36,16 @@ export class OrganizationListComponent implements OnInit {
         this.loadOrganizations();
     }
 
-    loadOrganizations(): void {
-        this.organizationService.getOrganizations().subscribe({
+    loadOrganizations(
+        page = this.page(),
+        size = this.pageSize()
+    ): void {
+        this.organizationService.getOrganizations(page, size).subscribe({
             next: (response) => {
                 this.organizations.set(response.data);
+                this.page.set(response.page);
+                this.pageSize.set(response.size);
+                this.total.set(response.total);
 
             },
             error: (error) => {
@@ -41,4 +53,13 @@ export class OrganizationListComponent implements OnInit {
             }
         })
     }
+
+    onPageChange(event: PageEvent): void {
+        this.loadOrganizations(
+            event.pageIndex,
+            event.pageSize
+        )
+    }
+
+
 }
