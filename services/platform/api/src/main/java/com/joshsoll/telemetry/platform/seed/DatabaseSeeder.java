785af17f7e7a850a0fcb09ff6ca7d3b054e25aa5
Josh Soll <jsdev305@gmail.com>
2026-07-26 18:18:28 -0400

    refactor: add device template generator

--- services/platform/api/src/main/java/com/joshsoll/telemetry/platform/seed/DatabaseSeeder.java
+++ services/platform/api/src/main/java/com/joshsoll/telemetry/platform/seed/DatabaseSeeder.java
@@ -4,6 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import com.joshsoll.telemetry.platform.organization.entity.Organization;
+import com.joshsoll.telemetry.platform.seed.devicetemplate.DeviceTemplateGenerator;
 import com.joshsoll.telemetry.platform.seed.organization.OrganizationGenerator;
 
 @Component
@@ -11,17 +13,28 @@ import com.joshsoll.telemetry.platform.seed.organization.OrganizationGenerator;
 public class DatabaseSeeder implements CommandLineRunner {
 
         private final OrganizationGenerator organizationGenerator;
+        private final DeviceTemplateGenerator deviceTemplateGenerator;
 
         public DatabaseSeeder(
-                        OrganizationGenerator organizationGenerator) {
+                        OrganizationGenerator organizationGenerator,
+                        DeviceTemplateGenerator deviceTemplateGenerator
+
+        ) {
 
                 this.organizationGenerator = organizationGenerator;
+                this.deviceTemplateGenerator = deviceTemplateGenerator;
 
         }
 
         @Override
         public void run(String... args) {
-                organizationGenerator.generate(5);
+                Organization organization = organizationGenerator.generate(
+                                "Organization 1",
+                                "organization-1");
+
+                deviceTemplateGenerator.generate(
+                                5,
+                                organization);
         }
 
 }
