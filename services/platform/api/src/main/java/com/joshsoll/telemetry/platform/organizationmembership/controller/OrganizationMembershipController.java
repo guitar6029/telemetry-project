6e4ca1a94c7a03108fd98f19f281aab7d3916d78
Josh Soll <jsdev305@gmail.com>
2026-07-26 12:38:49 -0400

    feat(api) add get requests for org membership  ,added exceptions

--- services/platform/api/src/main/java/com/joshsoll/telemetry/platform/organizationmembership/controller/OrganizationMembershipController.java
+++ services/platform/api/src/main/java/com/joshsoll/telemetry/platform/organizationmembership/controller/OrganizationMembershipController.java
@@ -1,13 +1,14 @@
 package com.joshsoll.telemetry.platform.organizationmembership.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.joshsoll.telemetry.platform.common.response.ApiResponse;
+import com.joshsoll.telemetry.platform.common.response.PagedApiResponse;
 import com.joshsoll.telemetry.platform.common.response.ResponseFactory;
 import com.joshsoll.telemetry.platform.organizationmembership.dto.CreateOrganizationMembershipRequest;
 import com.joshsoll.telemetry.platform.organizationmembership.dto.OrganizationMembershipResponse;
-
 import com.joshsoll.telemetry.platform.organizationmembership.service.OrganizationMembershipService;
 
 import jakarta.validation.Valid;
@@ -15,6 +16,7 @@ import jakarta.validation.Valid;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -39,6 +41,33 @@ public class OrganizationMembershipController {
                 .createOrganizationMembership(request);
 
         return ResponseFactory.created(organizationMembership, DOMAIN_NAME);
+    }
+
+    @GetMapping
+    public ResponseEntity<PagedApiResponse<OrganizationMembershipResponse>> getOrganizationMemberships(
+            @PathVariable UUID organizationId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+
+        PagedApiResponse<OrganizationMembershipResponse> organizationMemberships = organizationMembershipService
+                .getOrganizationMemberships(
+                        organizationId,
+                        page,
+                        size);
+
+        return ResponseFactory.ok(organizationMemberships);
+    }
+
+    @GetMapping("/{membershipId}")
+    public ResponseEntity<ApiResponse<OrganizationMembershipResponse>> getOrganizationMembership(
+            @PathVariable UUID organizationId,
+            @PathVariable UUID membershipId) {
+
+        OrganizationMembershipResponse membership = organizationMembershipService.getOrganizationMembership(
+                organizationId,
+                membershipId);
+
+        return ResponseFactory.ok(membership, null);
     }
 
 }
