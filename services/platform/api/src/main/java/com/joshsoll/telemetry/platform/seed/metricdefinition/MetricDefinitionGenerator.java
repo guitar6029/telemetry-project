60af7f797f5356276ed953a6fd98676ee5618899
Josh Soll <jsdev305@gmail.com>
2026-07-26 19:39:10 -0400

    refactor: add metric definition genetator

---
+++ services/platform/api/src/main/java/com/joshsoll/telemetry/platform/seed/metricdefinition/MetricDefinitionGenerator.java
@@ -0,0 +1,58 @@
+package com.joshsoll.telemetry.platform.seed.metricdefinition;
+
+import java.time.Instant;
+
+import org.springframework.stereotype.Component;
+
+import com.joshsoll.telemetry.platform.deviceTemplate.entity.DeviceTemplate;
+import com.joshsoll.telemetry.platform.metricDefinition.MetricDataType;
+import com.joshsoll.telemetry.platform.metricDefinition.entity.MetricDefinition;
+import com.joshsoll.telemetry.platform.metricDefinition.repository.MetricDefinitionRepository;
+
+@Component
+public class MetricDefinitionGenerator {
+    private final MetricDefinitionRepository metricDefinitionRepository;
+
+    public MetricDefinitionGenerator(
+            MetricDefinitionRepository metricDefinitionRepository) {
+        this.metricDefinitionRepository = metricDefinitionRepository;
+    }
+
+    public MetricDefinition generate(
+            String name,
+            String description,
+            String incomingFieldName,
+            MetricDataType dataType,
+            String unit,
+            DeviceTemplate deviceTemplate) {
+
+        Instant now = Instant.now();
+
+        MetricDefinition metricDefinition = new MetricDefinition(
+                name,
+                description,
+                incomingFieldName,
+                dataType,
+                unit,
+                deviceTemplate,
+                now,
+                now);
+
+        return metricDefinitionRepository.save(metricDefinition);
+    }
+
+    public void generate(
+            int count,
+            DeviceTemplate deviceTemplate) {
+
+        for (int i = 1; i <= count; i++) {
+            generate(
+                    "Metric Definition " + i,
+                    "Metric Definition Description " + i,
+                    "metric_" + i,
+                    MetricDataType.NUMBER,
+                    "unit-" + i,
+                    deviceTemplate);
+        }
+    }
+}
