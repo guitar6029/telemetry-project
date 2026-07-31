f6340125374b0eecbd4381eb24e56403fa2885ca
Josh Soll <jsdev305@gmail.com>
2026-07-31 18:06:09 -0400

    refactor(frontend): introduce application route structure and auth guard

--- services/platform/web/src/app/app.routes.ts
+++ services/platform/web/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { AUTH_ROUTES } from './features/auth/auth.routes';
 import { DashboardComponent } from './features/dashboard/pages/dashboard/dashboard.component';
+import { authGuard } from './core/auth.guard';
 
 export const routes: Routes = [
     {
@@ -8,23 +9,30 @@ export const routes: Routes = [
         children: AUTH_ROUTES
     },
     {
-        path: 'dashboard',
-        component: DashboardComponent
-    },
-    {
-        path: 'organizations',
-        loadChildren: () => import('./features/organization/organization.routes').then(m => m.ORGANIZATION_ROUTES)
-    },
-    {
-        path: 'manage',
+        path: 'app',
+        canActivate: [authGuard],
         children: [
             {
-                path: 'members',
-                loadChildren: () =>
-                    import('./features/organization-membership/organization-membership.routes')
-                        .then(m => m.ORGANIZATION_MEMBERSHIP_ROUTES)
+                path: 'dashboard',
+                component: DashboardComponent
             },
+            {
+                path: 'organizations',
+                loadChildren: () => import('./features/organization/organization.routes').then(m => m.ORGANIZATION_ROUTES)
+            },
+            {
+                path: 'manage',
+                children: [
+                    {
+                        path: 'members',
+                        loadChildren: () =>
+                            import('./features/organization-membership/organization-membership.routes')
+                                .then(m => m.ORGANIZATION_MEMBERSHIP_ROUTES)
+                    },
 
+                ]
+            }
         ]
     }
+
 ];
