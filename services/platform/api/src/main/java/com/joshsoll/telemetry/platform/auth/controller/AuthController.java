f81fe80fab2d2909720cd80f1b2fc25a6f7a3be9
Josh Soll <jsdev305@gmail.com>
2026-07-31 17:00:16 -0400

    added noContent to response factory

--- services/platform/api/src/main/java/com/joshsoll/telemetry/platform/auth/controller/AuthController.java
+++ services/platform/api/src/main/java/com/joshsoll/telemetry/platform/auth/controller/AuthController.java
@@ -3,14 +3,18 @@ package com.joshsoll.telemetry.platform.auth.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.joshsoll.telemetry.platform.auth.constants.JwtConstants;
 import com.joshsoll.telemetry.platform.auth.constants.UserConstants;
 import com.joshsoll.telemetry.platform.auth.dto.LoginRequest;
 import com.joshsoll.telemetry.platform.auth.dto.RegisterRequest;
+import com.joshsoll.telemetry.platform.auth.exception.MissingAccessTokenException;
 import com.joshsoll.telemetry.platform.auth.service.AuthService;
 import com.joshsoll.telemetry.platform.common.api.ApiRoutes;
 import com.joshsoll.telemetry.platform.common.response.ApiResponse;
 import com.joshsoll.telemetry.platform.common.response.ResponseFactory;
+import com.joshsoll.telemetry.platform.security.util.CookieUtils;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 import org.springframework.http.HttpHeaders;
@@ -57,6 +61,17 @@ public class AuthController {
                 null,
                 "Login Successful",
                 headers);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletRequest request) {
+
+        String token = CookieUtils.getCookieValue(request, JwtConstants.COOKIE_NAME)
+                .orElseThrow(() -> new MissingAccessTokenException());
+
+        authService.logout(token);
+
+        return ResponseFactory.noContent();
     }
 
 }
