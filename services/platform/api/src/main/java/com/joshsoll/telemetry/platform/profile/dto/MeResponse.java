d031d1013bacb4e3a239f14a285b8798b5654c97
Josh Soll <jsdev305@gmail.com>
2026-07-31 22:48:05 -0400

    updated MeResponse, fixed wrong property, now sending correct organization id

--- services/platform/api/src/main/java/com/joshsoll/telemetry/platform/profile/dto/MeResponse.java
+++ services/platform/api/src/main/java/com/joshsoll/telemetry/platform/profile/dto/MeResponse.java
@@ -3,10 +3,10 @@ package com.joshsoll.telemetry.platform.profile.dto;
 import java.util.UUID;
 
 public record MeResponse(
-                UUID orgId,
-                String firstName,
-                String lastName,
-                String email,
-                String avatarUrl) {
+        UUID organizationId,
+        String firstName,
+        String lastName,
+        String email,
+        String avatarUrl) {
 
 }
