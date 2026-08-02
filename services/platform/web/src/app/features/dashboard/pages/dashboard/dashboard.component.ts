e05498bd6cbe29893c47c494dfa3d646a079785c
Josh Soll <jsdev305@gmail.com>
2026-08-02 18:11:48 -0400

    add test logout button with event

--- services/platform/web/src/app/features/dashboard/pages/dashboard/dashboard.component.ts
+++ services/platform/web/src/app/features/dashboard/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../../auth/service/auth.service';
+import { SessionService } from '../../../auth/service/session.service';
 
 
 @Component({
@@ -8,4 +10,13 @@ import { RouterLink } from '@angular/router';
     templateUrl: './dashboard.component.html',
     styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent { }
+export class DashboardComponent {
+
+
+    private readonly sessionService = inject(SessionService);
+
+    logout() {
+        this.sessionService.logout();
+    }
+
+}
