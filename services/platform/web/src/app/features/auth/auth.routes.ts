990f0f22c1d349ea49b50fbbf518da2ad2c2507f
Josh Soll <jsdev305@gmail.com>
2026-08-03 14:39:36 -0400

    removed register route

--- services/platform/web/src/app/features/auth/auth.routes.ts
+++ services/platform/web/src/app/features/auth/auth.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from "@angular/router";
 import { AuthLayoutComponent } from "./components/auth-layout/auth-layout.component";
 import { LoginComponent } from "./pages/login/login.component";
-import { RegisterComponent } from "./pages/register/register.component";
 
 export const AUTH_ROUTES: Routes = [
     {
@@ -11,10 +10,6 @@ export const AUTH_ROUTES: Routes = [
             {
                 path: 'login',
                 component: LoginComponent
-            },
-            {
-                path: 'register',
-                component: RegisterComponent,
             }
         ]
     }
