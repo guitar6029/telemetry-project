ce13da06b8d099e54431359a5938db78ad6ae794
Josh Soll <jsdev305@gmail.com>
2026-08-06 12:06:26 -0400

    add DeviceOverview feature

---
+++ services/platform/api/src/main/java/com/joshsoll/telemetry/platform/deviceoverview/dto/DevicesOverviewResponse.java
@@ -0,0 +1,6 @@
+package com.joshsoll.telemetry.platform.deviceoverview.dto;
+
+public record DevicesOverviewResponse(
+        long templateCount,
+        long deviceCount) {
+}
---
+++ services/platform/api/src/main/java/com/joshsoll/telemetry/platform/deviceoverview/service/DeviceOverviewService.java
@@ -0,0 +1,49 @@
+package com.joshsoll.telemetry.platform.deviceoverview.service;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.joshsoll.telemetry.platform.auth.entity.User;
+import com.joshsoll.telemetry.platform.auth.service.AuthorizationService;
+import com.joshsoll.telemetry.platform.device.repository.DeviceRepository;
+import com.joshsoll.telemetry.platform.deviceoverview.dto.DevicesOverviewResponse;
+import com.joshsoll.telemetry.platform.devicetemplate.repository.DeviceTemplateRepository;
+import com.joshsoll.telemetry.platform.organization.entity.Organization;
+
+@Service
+
+public class DeviceOverviewService {
+
+    private final AuthorizationService authorizationService;
+    private final DeviceTemplateRepository deviceTemplateRepository;
+    private final DeviceRepository deviceRepository;
+
+    public DeviceOverviewService(
+            AuthorizationService authorizationService,
+            DeviceTemplateRepository deviceTemplateRepository,
+            DeviceRepository deviceRepository
+
+    ) {
+        this.authorizationService = authorizationService;
+        this.deviceTemplateRepository = deviceTemplateRepository;
+        this.deviceRepository = deviceRepository;
+    }
+
+    public DevicesOverviewResponse getDevicesOverview(User authenticatedUser, UUID organizationId) {
+
+        Organization organization = authorizationService.requireOrganizationAccess(authenticatedUser, organizationId);
+
+        long numberOfDeviceTemplates = deviceTemplateRepository.countByOrganizationId(organization.getId());
+
+        long numberOfDevices = deviceRepository.countByOrganizationId(organization.getId());
+
+        DevicesOverviewResponse deviceOverview = new DevicesOverviewResponse(
+                numberOfDeviceTemplates,
+                numberOfDevices);
+
+        return deviceOverview;
+
+    }
+
+}
