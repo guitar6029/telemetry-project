50ce1fafff5c63d1f598c22b00dcb76c5579c45d
Josh Soll <jsdev305@gmail.com>
2026-08-06 12:18:27 -0400

    add RouterLink

--- services/platform/web/src/app/components/side-nav/pages/side-nav.component.ts
+++ services/platform/web/src/app/components/side-nav/pages/side-nav.component.ts
@@ -1,8 +1,10 @@
 import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
 
 
 @Component({
     selector: 'telemetry-side-nav',
+    imports: [RouterLink],
     templateUrl: './side-nav.component.html',
     styleUrl: './side-nav.component.scss'
 })
