4a467992052feba34fdf95ad903dde263102e76d
Josh Soll <jsdev305@gmail.com>
2026-08-06 12:06:04 -0400

    add long countByOrganizationId

--- services/platform/api/src/main/java/com/joshsoll/telemetry/platform/device/repository/DeviceRepository.java
+++ services/platform/api/src/main/java/com/joshsoll/telemetry/platform/device/repository/DeviceRepository.java
@@ -14,4 +14,6 @@ public interface DeviceRepository extends JpaRepository<Device, UUID> {
             String serialNumber);
 
     Optional<Device> findBySerialNumber(String serialNumber);
+
+    long countByOrganizationId(UUID organizationId);
 }
