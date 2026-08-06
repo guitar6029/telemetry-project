68c6c5041cbfe3b24dc229727e27212da4281a63
Josh Soll <jsdev305@gmail.com>
2026-08-05 20:18:05 -0400

    add static splash loading screen

--- services/platform/web/src/app/app.ts
+++ services/platform/web/src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SessionService } from './features/auth/service/session.service';
 
 @Component({
   selector: 'telemetry-root',
@@ -7,6 +8,13 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('telemetry');
+  protected readonly sessionService = inject(SessionService);
+
+  ngOnInit(): void {
+    const splash = document.getElementById('startup-loading');
+
+    splash?.remove();
+  }
 }
