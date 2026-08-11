41f1665646063da5d9570c99f5c2d304cc7ace73
Josh Soll <jsdev305@gmail.com>
2026-08-11 16:21:30 -0400

    feat: refactor organization selection to use telemetry-select component and add utility for select options

--- services/platform/web/src/app/components/organization-selection/pages/organization-selection.component.ts
+++ services/platform/web/src/app/components/organization-selection/pages/organization-selection.component.ts
@@ -1,10 +1,12 @@
-import { Component, inject } from "@angular/core";
+import { Component, computed, inject } from "@angular/core";
 import { OrganizationContextStore } from "../../../core/stores/organization-context.store";
 import { FormsModule } from "@angular/forms";
+import { SelectComponent } from "../../select/select.component";
+import { toSelectOptions } from "../../../utils/to-select-option";
 
 @Component({
     selector: 'telemetry-organization-selection',
-    imports: [FormsModule],
+    imports: [FormsModule, SelectComponent],
     templateUrl: './organization-selection.component.html',
     styleUrl: './organization-selection.component.scss'
 })
@@ -14,6 +16,28 @@ export class OrganizationSelectionComponent {
     private readonly organizationContext = inject(OrganizationContextStore);
     readonly organizations = this.organizationContext.organizations;
     readonly currentOrganization = this.organizationContext.currentOrganization;
+
+
+    readonly currentOrganizationOption = computed(() => {
+        const organization = this.currentOrganization();
+        if (!organization) {
+            return null;
+        }
+
+        return {
+            label: organization.name,
+            value: organization.id
+        };
+    });
+
+    readonly organizationOptions = computed(() => {
+        return toSelectOptions(this.organizations(),
+            organization => organization.name,
+            organization => organization.id,
+        )
+    })
+
+
 
     changeOrganization(organizationId: string): void {
         this.organizationContext.changeCurrentOrganization(organizationId);
