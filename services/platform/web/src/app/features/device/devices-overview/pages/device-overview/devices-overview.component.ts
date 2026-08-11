4b6a460a9dee32b7c7d6303593ba5ef96e97dd3c
Josh Soll <jsdev305@gmail.com>
2026-08-11 17:41:40 -0400

    add page component for easier root page style management

--- services/platform/web/src/app/features/device/devices-overview/pages/device-overview/devices-overview.component.ts
+++ services/platform/web/src/app/features/device/devices-overview/pages/device-overview/devices-overview.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { DevicesSummaryComponent } from "../../components/device-summary/pages/devices-summary.component";
+import { PageComponent } from "../../../../../components/page/page.component";
 
 @Component({
     selector: 'telemetry-devices-overview',
     templateUrl: './devices-overview.component.html',
     styleUrl: './devices-overview.component.scss',
-    imports: [DevicesSummaryComponent]
+    imports: [DevicesSummaryComponent, PageComponent]
 })
 
 export class DevicesOverviewComponent {
