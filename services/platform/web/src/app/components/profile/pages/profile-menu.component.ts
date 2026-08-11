c22d5c099288ead11449e1fc95f52b49f7bbaf41
Josh Soll <jsdev305@gmail.com>
2026-08-11 17:08:12 -0400

    updated profile component

--- services/platform/web/src/app/components/profile/pages/profile-menu.component.ts
+++ services/platform/web/src/app/components/profile/pages/profile-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { ProfileStore } from "../../../core/stores/profile.store";
 import { SessionService } from "../../../features/auth/service/session.service";
 
@@ -16,12 +16,6 @@ export class ProfileMenuComponent {
     readonly fullName = this.profileStore.fullName;
     readonly email = this.profileStore.email;
     readonly avatarUrl = this.profileStore.avatarUrl;
-
-    dropdownDisplaying = signal(false)
-
-    toggleDropdown(): void {
-        this.dropdownDisplaying.set(!this.dropdownDisplaying());
-    }
 
     logout(): void {
         this.sessionService.logout();
