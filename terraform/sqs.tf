1bcc2749007045e6c3b2df5b5118661fc7974b15
Josh Soll <jsdev305@gmail.com>
2026-08-25 20:10:01 -0400

    feat: add visibility timeout to device import SQS queue

--- terraform/sqs.tf
+++ terraform/sqs.tf
@@ -1,6 +1,8 @@
 resource "aws_sqs_queue" "device_import" {
     name = "device-import"
 
+    visibility_timeout_seconds = 300
+
     redrive_policy = jsonencode({
         deadLetterTargetArn = aws_sqs_queue.device_import_dlq.arn
         maxReceiveCount     = 3
