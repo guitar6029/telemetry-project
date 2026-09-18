4d38838a67eb4fa83b798a66e7f0ea384def6c20
guitar6029 <109701970+guitar6029@users.noreply.github.com>
2026-09-17 20:19:19 -0400

    Configure JSON message converter for RabbitMQ

--- services/platform/api/src/main/java/com/joshsoll/telemetry/platform/device/importer/config/DeviceImportRabbitConfig.java
+++ services/platform/api/src/main/java/com/joshsoll/telemetry/platform/device/importer/config/DeviceImportRabbitConfig.java
@@ -1,6 +1,8 @@
 package com.joshsoll.telemetry.platform.device.importer.config;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,5 +14,10 @@ public class DeviceImportRabbitConfig {
     @Bean
     public Queue deviceImportQueue() {
         return new Queue(DeviceImportConstants.DEVICE_IMPORT_QUEUE_NAME, true);
+    }
+
+    @Bean
+    public MessageConverter rabbitMessageConverter() {
+        return new JacksonJsonMessageConverter();
     }
 }
